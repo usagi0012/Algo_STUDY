862e2cb711faab23cc3d0cb3d1cbac62624024a1
nohsiwon <98876@naver.com>
2023-11-09 15:52:25 +0900

    41번 문제

--- 이상한 문자 만들기.js
+++ 이상한 문자 만들기.js
@@ -1,1 +1,21 @@
-console.log('Hello World');
+function solution(s) {
+    let a = '';
+    let b = s.split(' ');
+
+    for (let i = 0; i < b.length; i++) {
+        for (let j = 0; j < b[i].length; j++) {
+            if (j % 2 == 0) {
+                a += b[i][j].toUpperCase();
+            }
+            if (j % 2 == 1) {
+                a += b[i][j].toLowerCase();
+            }
+        }
+        if (i + 1 !== b.length) {
+            a += ' ';
+        } else {
+            a += '';
+        }
+    }
+    return a;
+}
