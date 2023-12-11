477c97ab28e9c91e7f1f1eeff87c7402ffd0bb5c
visitor17564 <lanlanloo17564@gmail.com>
2023-12-11 18:09:19 +0900

    72번 업데이트

---
+++ 72.js
@@ -0,0 +1,23 @@
+let players = ["mumu", "soe", "poe", "kai", "mine"];
+let callings = ["kai", "kai", "mine", "mine"];
+
+function solution(players, callings) {
+  let answer = players;
+  const mapAnswer = new Map();
+  for (a = 0; a < answer.length; a++) {
+    mapAnswer.set(answer[a], a);
+  }
+  mapAnswer;
+  for (a = 0; a < callings.length; a++) {
+    let callIndex = mapAnswer.get(callings[a]);
+    let now = answer[callIndex];
+    let before = answer[callIndex - 1];
+    answer[callIndex] = before;
+    mapAnswer.set(before, callIndex);
+    answer[callIndex - 1] = now;
+    mapAnswer.set(now, callIndex - 1);
+  }
+  return answer;
+}
+
+solution(players, callings);
