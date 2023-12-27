021f1de6ce2fc980764def19dc76f6ee2e6753d6
visitor17564 <lanlanloo17564@gmail.com>
2023-12-27 09:53:54 +0900

    [add] 78번까지

---
+++ 75.js
@@ -0,0 +1,11 @@
+let s = "-1 -2 -3 -4"
+
+function solution(s) {
+    let answer = '';
+    // 일단 숫자를 배열로 반환?
+    let arr = s.split(' ').sort((a,b) => (Number(a)-Number(b)))
+    answer += `${arr[0]} ${arr[arr.length-1]}`
+    return answer;
+}
+
+solution(s)
---
+++ 77.js
@@ -0,0 +1,25 @@
+let s = "01110"
+let a = s.indexOf("0")
+console.log(a)
+
+
+function solution(s) {
+    let answer = [];
+    let count = 0
+    let deletedZero = 0
+    let next = s
+    while(next.length!==1){
+        count++
+        if(next.indexOf("0")===-1){
+            next = next.split("0").join("").length.toString(2)
+        } else {
+            deletedZero += next.match(/0/g).length
+            next = next.split("0").join("").length.toString(2)
+        }
+    }
+    answer.push(count)
+    answer.push(deletedZero)
+    return answer;
+}
+
+solution(s)
