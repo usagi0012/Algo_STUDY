02f3691dda4ad9f81dc12a996f540d5b3609fc7b
visitor17564 <lanlanloo17564@gmail.com>
2024-01-04 09:59:28 +0900

    [add] 81번까지

---
+++ 80.js
@@ -0,0 +1,16 @@
+let n = 8
+let a = 4
+let b = 7
+
+function solution(n,a,b)
+{
+    let answer = 1;
+    while(Math.ceil(a/2)!==Math.ceil(b/2)){
+        answer++;
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+    }
+    return answer;
+}
+
+solution(n,a,b)
---
+++ 81.js
@@ -0,0 +1,22 @@
+let arr = [4,5]
+
+function solution(arr) {
+  let gcd = function(a, b) {
+      if (b === 0) {
+          return a;
+      }
+      return gcd(b, a % b);
+  }
+
+  let lcm = function(a, b) {
+      return (a * b) / gcd(a, b);
+  }
+
+  let answer = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+      answer = lcm(arr[i], answer);
+  }
+  return answer;
+}
+
+solution(arr)
